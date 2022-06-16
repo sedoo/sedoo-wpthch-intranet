8b70ba474a85e5ee5cdf256cd168f94d835d6873
Pierre VERT <vert@obs-mip.fr>
2022-06-16 17:53:53 +0200

    prepare function sedoo_intranet_change_usergroup()

--- inc/intranet-user-hooks.php
+++ inc/intranet-user-hooks.php
@@ -5,10 +5,11 @@
  */
 
  
-function your_function( $user_login, $user ) {
-    // your code
+function sedoo_intranet_change_usergroup( $user_login, $user ) {
+    //get user_mail, explode domain
+    // switch case 
+        // update group id in  jdy0b2re_64_groups_user_group for user_id
 }
-//add_action('wp_login', 'your_function', 10, 2);
-
+// add_action('wp_login', 'sedoo_intranet_change_usergroup', 10, 2);
 
 ?>
