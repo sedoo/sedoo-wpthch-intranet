c1d8dde33fc67da5adfb906556a34dfae183e285
Pierre VERT <vert@obs-mip.fr>
2022-06-21 16:09:18 +0200

    auto add user to blog

--- inc/intranet-user-hooks.php
+++ inc/intranet-user-hooks.php
@@ -6,20 +6,13 @@
 // sedoo_intranet_change_usergroup('pvert', Object(WP_User)
 
 function sedoo_intranet_change_usergroup( $user_login, WP_User $user ) {
-    //get user_mail, explode domain
 
-// function sedoo_intranet_change_usergroup( $user, $username, $password ) {
-
+    // ldap connect
     $ldaphost ="ldap://195.83.20.90";
     $ldapport = 389;
 
     $ds = ldap_connect($ldaphost, $ldapport)
     or die("Could not connect to $ldaphost");
-
-    // $person="pvert";
-    // $user_id=get_current_user_id();
-    // $user_info=get_userdata($user->ID);
-    // $uid=$user_info->user_login;
     
     $dn = "ou=people,dc=omp";
     // $filter="(|(uid=".$uid."*)(givenName=".$person."*))";
@@ -33,37 +26,34 @@ function sedoo_intranet_change_usergroup( $user_login, WP_User $user ) {
     $gidnumber=$info[0]['gidnumber'][0];
     
     switch ($gidnumber) {
-    // case 'irap.omp.eu':
-    case '1200':
+    case '1200':    //IRAP
         $group_id=2;
-        
         break;
-    case '900':
+
+    case '900':     //UAR
         $group_id=3;
-        
         break;
-    // case 'cesbio.cnes.fr':
-    case '600':
+
+    case '600':     //CESBIO
         $group_id=4;
-        
         break;
-    // case 'get.omp.eu':
-    case '700':
-        $group_id=5;
-        
+    
+    case '700':     //GET
+        $group_id=5;   
         break;
-    // case 'aero.obs-mip.fr':
-    case '1000':  
-        $group_id=6;
-        
+    
+    case '1000':    //LAERO
+        $group_id=6; 
         break;
-    // case 'legos.obs-mip.fr':
-    case '1100':
-        $group_id=7;
-        
+    
+    case '1100':    //LEGOS
+        $group_id=7; 
+        break;
+
+    case '800':    //LEFE
+        $group_id=9; 
         break;
     }
-    // echo "ENTRY WP GROUP: ".$group_id;
     // update group id in  prefix_groups_user_group for user_id
     global $wpdb;
     $table = $wpdb->prefix.'groups_user_group';
@@ -74,8 +64,16 @@ function sedoo_intranet_change_usergroup( $user_login, WP_User $user ) {
     $where_format = array('%d');
     $wpdb->update( $table, $data, $where, $format, $where_format );
 
+    //ADD USER ID  TO CURRENT BLOG ID AS AN EDITOR
+    // $user_id = 1; 
+    $blog_id = get_current_blog_id();
+    $role = 'subscriber';
+    // only if user is not in current blog
+    if ( is_user_member_of_blog( $user->ID, $blog_id ) ) {
+
+        add_user_to_blog( $blog_id, $user->ID, $role );
+    }
 }
 add_action('wp_login', 'sedoo_intranet_change_usergroup', 10, 2);
-// add_filter( 'authenticate', 'sedoo_intranet_change_usergroup', 30, 3 );
 
 ?>
