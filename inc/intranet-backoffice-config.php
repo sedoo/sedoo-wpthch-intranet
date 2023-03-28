039d8e2651213b6da2e171ce946f7703f9ebd423
Pierre VERT <vert@obs-mip.fr>
2023-03-28 16:14:57 +0200

    fix capability

--- inc/intranet-backoffice-config.php
+++ inc/intranet-backoffice-config.php
@@ -12,7 +12,7 @@ function sedoo_intranet_menu() {
      'sedoo-intranet-admin-main-page', ''); // in sedoo-intranet-mainadmin.php
 }
 
-if (!current_user_can( 'edit_posts' )) {
+if (!current_user_can( 'edit_pages' )) {
 	add_filter('show_admin_bar', '__return_false');
 }
 // END THE MAIN ADMINISTRATION PAGE
